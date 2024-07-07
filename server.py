ce8b7ab258c285e0c53611f300136ddfe0b49c24
Da-OOn <130433383+Da-OOn@users.noreply.github.com>
2024-07-08 03:21:26 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
 import socket
-
 def start_server():
-    host = '0.0.0.0'  # Bind to all interfaces
+    host = '0.0.0.0'
     port = 12345
-
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
     s.listen(1)
-
+    print("Server is listening...")
+    conn, addr = s.accept()
+    print(f"Connected by {addr}")
     while True:
-        conn, addr = s.accept()
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print("Received: ", repr(data))
-
-start_server()
+        data = conn.recv(1024)
+        if not data:
+            break
+        result = data.decode()  # 받은 데이터를 문자열로 변환
+        print("Received result: ", result)
+    conn.close()
+    s.close()
+try:
+    start_server()
+except KeyboardInterrupt:
+    print("Server stopped by User")
